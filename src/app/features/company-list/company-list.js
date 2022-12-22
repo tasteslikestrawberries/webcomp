58dd9c8b427b49a147b4b8787db884aae23e95fc
Anja Mustać <73844750+tasteslikestrawberries@users.noreply.github.com>
2022-12-22 13:25:32 +0100

    refactor

--- src/app/features/company-list/company-list.js
+++ src/app/features/company-list/company-list.js
@@ -26,7 +26,7 @@ class CompanyList extends HTMLElement {
       const loadingSpinner = document.createElement("loading-spinner");
       this.shadowRoot.appendChild(loadingSpinner);
     } else {
-      const loadingSpinner = this.shadowRoot.querySelector("loading-spinner");
+      const loadingSpinner = this.findElement("loading-spinner");
       this.shadowRoot.removeChild(loadingSpinner);
     }
   }
@@ -41,50 +41,114 @@ class CompanyList extends HTMLElement {
     });
   }
 
+  findElement(className, templateClone) {
+    if (templateClone) {
+      return templateClone.querySelector(className);
+    }
+    return this.shadowRoot.querySelector(className);
+  }
+
+  renderCompanyList(data) {
+    const ulElement = this.findElement(".company-list");
+    const companyTemplate = this.findElement(".company-template");
+
+    data.result.map(({ name, id }) => {
+      const companyTemplateClone = companyTemplate.content.cloneNode(true);
+
+      const companyItem = this.findElement(
+        ".company-item",
+        companyTemplateClone
+      );
+      const websitesList = this.findElement(
+        ".website-list",
+        companyTemplateClone
+      );
+      const companyName = this.findElement(
+        ".company-item--name",
+        companyTemplateClone
+      );
+      const caretIcon = this.findElement(".icon-caret", companyTemplateClone);
+
+      companyItem.addEventListener("click", this.onToggleCompany.bind(this));
+
+      companyItem.classList.add(`company-item-${id}`);
+      companyItem.dataset.isExpanded = false;
+
+      companyItem.dataset.id = id;
+      companyName.textContent = name;
+
+      websitesList.classList.add(`website-list-${id}`);
+      caretIcon.classList.add(`icon-caret-${id}`);
+      websitesList.id = `website-list-${id}`;
+
+      ulElement.appendChild(companyTemplateClone);
+    });
+  }
+
+  onToggleCompany(ev) {
+    this.renderCompanyWebsites(ev.currentTarget.dataset.id);
+  }
+
   renderCompanyWebsites(id) {
-    const websitesList = this.shadowRoot.querySelector(`.website-list-${id}`);
-    websitesList.style.display =
-      websitesList.style.display === "block" ? "none" : "block";
+    const websitesList = this.findElement(`.website-list-${id}`);
+    const caretIcon = this.findElement(`.icon-caret-${id}`);
+    if (websitesList.style.display === "block") {
+      websitesList.style.display = "none";
+      caretIcon.classList.remove("with-rotate");
+      caretIcon.style.borderLeftColor = "#828282";
+    } else {
+      websitesList.style.display = "block";
+      caretIcon.classList.add("with-rotate");
+      caretIcon.style.borderLeftColor = "#2f80ed";
+    }
 
     if (websitesList.children.length !== 0) {
       return;
     }
 
-    const websiteTemplate = this.shadowRoot.querySelector(".website-template");
+    const websiteTemplate = this.findElement(".website-template");
 
     const company = this.data.result.find(
       (company) => id === company.id.toString()
     );
+
     company.websites.forEach(({ id, name, sections, status }) => {
       const websiteTemplateClone = websiteTemplate.content.cloneNode(true);
-      const websiteItem = websiteTemplateClone.querySelector(".website-item");
-      const websiteName = websiteTemplateClone.querySelector(
-        ".website-item--name"
+      const websiteItem = this.findElement(
+        ".website-item",
+        websiteTemplateClone
       );
-      const websiteSectionsCount = websiteTemplateClone.querySelector(
-        ".website-item--sections-count"
+      const websiteName = this.findElement(
+        ".website-item--name",
+        websiteTemplateClone
       );
-      const websiteStatus = websiteTemplateClone.querySelector(
-        ".website-item--status"
+      const websiteSectionsCount = this.findElement(
+        ".website-item--sections-count",
+        websiteTemplateClone
       );
-      const websiteStatusIndicator = websiteTemplateClone.querySelector(
-        ".website-item--status-indicator"
+      const websiteStatus = this.findElement(
+        ".website-item--status",
+        websiteTemplateClone
       );
+      const websiteStatusIndicator = this.findElement(
+        ".website-item--status-indicator",
+        websiteTemplateClone
+      );
+
+      websiteItem.classList.add(`website-item-${id}`);
       websiteItem.classList.add(`website-item-${id}`);
       websiteName.textContent = name;
       websiteSectionsCount.textContent = sections?.length ?? 0;
+
       const { label, color } = this.getWebsiteStatus(status) ?? {};
       websiteStatus.textContent = label;
+
       websiteStatusIndicator.style.backgroundColor = color;
       if (color === "white") {
         websiteStatusIndicator.style.border = "1px solid #333333";
       }
       websitesList.appendChild(websiteItem);
     });
-  }
-
-  onToggleCompany(e) {
-    this.renderCompanyWebsites(e.currentTarget.dataset.id);
   }
 
   getWebsiteStatus(status) {
@@ -96,34 +160,6 @@ class CompanyList extends HTMLElement {
       4: { label: "Pending", color: "yellow" },
       5: { label: "Stopped", color: "#BDBDBD" },
     }[status];
-  }
-
-  renderCompanyList(data) {
-    const ulElement = this.shadowRoot.querySelector(".company-list");
-    const companyTemplate = this.shadowRoot.querySelector(".company-template");
-
-    data.result.map(({ name, id }) => {
-      const companyTemplateClone = companyTemplate.content.cloneNode(true);
-
-      const companyItem = companyTemplateClone.querySelector(".company-item");
-      const websitesList = companyTemplateClone.querySelector(".website-list");
-      const companyName = companyTemplateClone.querySelector(
-        ".company-item--name"
-      );
-
-      companyItem.addEventListener("click", this.onToggleCompany.bind(this));
-
-      companyItem.classList.add(`company-item-${id}`);
-      companyItem.dataset.isExpanded = false;
-
-      companyItem.dataset.id = id;
-      companyName.textContent = name;
-
-      websitesList.classList.add(`website-list-${id}`);
-      websitesList.id = `website-list-${id}`;
-
-      ulElement.appendChild(companyTemplateClone);
-    });
   }
 }
 
